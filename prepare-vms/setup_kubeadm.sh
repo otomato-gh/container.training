447c66dd7c82b90e594cac86250a701a51937fc5
Anton Weiss <ant.weiss@gmail.com>
2018-02-27 12:43:18 +0200

    kubeadm setup script

---
+++ prepare-vms/setup_kubeadm.sh
@@ -0,0 +1,37 @@
+#! /bin/bash -f
+
+sudo apt-get update
+
+#Install packages to allow apt to use a repository over HTTPS:
+sudo apt-get install -y apt-transport-https \
+	                ca-certificates \
+		        curl \
+		        software-properties-common
+
+#Add Docker’s official GPG key:
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	      $(lsb_release -cs) \
+	         stable"
+
+sudo apt-get update && sudo apt-get install -y docker-ce
+
+#install compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+
+sudo adduser $USER docker
