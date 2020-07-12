2583ec4f1be043eb2a98afc1b8b302ad8c7e1849
Anton Weiss <anton@otomato.io>
2020-07-12 11:13:33 +0300

    update kubectl-run.md

--- prepare-vms/setup_minikube_sn_ub1804.sh
+++ prepare-vms/setup_minikube_sn_ub1804.sh
@@ -26,7 +26,7 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 
 #install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.14.4/bin/linux/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl
 
 #Make the kubectl binary executable.
 chmod +x ./kubectl
@@ -37,9 +37,10 @@ sudo mv ./kubectl /usr/bin/kubectl
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 
 sudo adduser $USER docker
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.1.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
 
-sudo minikube start --vm-driver=none --kubernetes-version=v1.14.4 --extra-config=kubeadm.ignore-preflight-errors=SystemVerification --extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf --extra-config=kubeadm.ignore-preflight-errors=NumCPU
+sudo minikube start --vm-driver=none --kubernetes-version=v1.18.0 --extra-config=kubeadm.ignore-preflight-errors=SystemVerification --extra-config=kubelet.resolv-conf=/run/systemd/resolve/resolv.conf --extra-config=kubeadm.ignore-preflight-errors=NumCPU
 
 sudo mv /root/.kube $HOME/.kube # this will write over any previous configuration
 sudo chown -R $USER $HOME/.kube
